95134968ad743afcc2330154927ce698c2f01dd6
Adrian <adriantutuianu01@gmail.com>
2022-09-29 20:02:02 +0100

    Added cat logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [cat, setCat] = useState("");
+
   const getCat = async () => {
     try {
       await fetch("http://thecatapi.com/api/images/get?format=src&type=gif")
         .then((result) => result.json())
         .then((result) => {
           console.log(result);
+          // setCat - result
+          setCat(result);
         });
     } catch (error) {
       console.log("Failed to retrieve the quote: " + error);
@@ -31,6 +36,11 @@ function App() {
         </a>
       </header>
       <button onClick={getCat}>Get Cat</button>
+      <img
+        src={cat}
+        alt="cat"
+        style={{ width: "200px", height: "200px" }}
+      ></img>
     </div>
   );
 }
